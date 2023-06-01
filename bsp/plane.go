e427266f6429b0d30769672432d3ae0dd0094995
Joe Reid <joe@joereid.com>
2023-05-31 22:13:59 -0400

    plane parsing

--- bsp/plane.go
+++ bsp/plane.go
@@ -1,20 +1,34 @@
 package bsp
 
+import (
+	"github.com/packetflinger/libq2/types"
+)
+
 const (
 	BSPPlaneSize = 20
 )
 
 type BSPPlane struct {
-	Data []byte
+	Normal   types.Vector3
+	Distance int32
+	Type     int32
 }
 
 func (bsp *BSPFile) FetchPlanes() []BSPPlane {
 	plaincount := bsp.LumpMeta[PlanesLump].length / BSPPlaneSize
 	planes := []BSPPlane{}
 
+	msg := &bsp.LumpData[PlanesLump].Data
+	msg.Index = 0
 	for i := 0; i < int(plaincount); i++ {
 		planes = append(planes, BSPPlane{
-			Data: bsp.LumpData[PlanesLump].Data.ReadData(20),
+			Normal: types.Vector3{
+				X: msg.ReadLong(),
+				Y: msg.ReadLong(),
+				Z: msg.ReadLong(),
+			},
+			Distance: msg.ReadLong(),
+			Type:     msg.ReadLong(),
 		})
 	}
 	return planes
---
+++ types/vector.go
@@ -0,0 +1,14 @@
+package types
+
+type Vector3 struct {
+	X int32
+	Y int32
+	Z int32
+}
+
+type Vector2 struct {
+	X int32
+	Y int32
+}
+
+type Vector uint16
