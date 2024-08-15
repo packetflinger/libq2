c3e5dec41c87377f492365f6243260a6b7896f14
Joe Reid <joe@joereid.com>
2024-08-14 21:55:42 -0400

    use protos for this

--- examples/demos/prints.go
+++ examples/demos/prints.go
@@ -7,29 +7,36 @@ import (
 	"log"
 
 	"github.com/packetflinger/libq2/demo"
-	"github.com/packetflinger/libq2/message"
-	"github.com/packetflinger/libq2/util"
 )
 
 func main() {
 	// open the demo file
-	demo, err := demo.OpenDM2File("../testdata/testduel.dm2")
+	dm2, err := demo.NewDM2Demo("../../testdata/testduel.dm2")
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
+	err = dm2.Unmarshal()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	for _, frame := range dm2.GetTextProto().GetFrames() {
+		fmt.Println("frame!")
+		for _, print := range frame.GetPrints() {
+			fmt.Println(print.GetString_())
+		}
+	}
 	// set a callback for parsing prints
-	callback := message.MessageCallbacks{
+	/*callback := message.MessageCallbacks{
 		Print: func(p *message.Print) {
 			fmt.Println(util.ConvertHighChars(p.String[:len(p.String)-1]))
 		},
-	}
+	}*/
 
 	// parse all demo messages running our callback function
 	// every time a print message is found
-	demo.ParseDM2(callback)
-
-	// clean up
-	demo.Close()
+	//demo.ParseDM2(callback)
 }
