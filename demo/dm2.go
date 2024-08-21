2bb2a8d45bfbd7369d6873b4829fef517db7fd29
Joe Reid <joe@joereid.com>
2024-08-21 15:11:15 -0400

    Track the current frame when parsing

--- demo/dm2.go
+++ demo/dm2.go
@@ -1,6 +1,7 @@
 package demo
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
@@ -14,6 +15,7 @@ type DM2Demo struct {
 	textProto      *pb.DM2Demo
 	binaryData     []byte // the contents of a .dm2 file
 	binaryPosition int    // where in those contents we are
+	currentFrame   *pb.Frame
 }
 
 // Read the entire binary demo file into memory
@@ -27,6 +29,7 @@ func NewDM2Demo(filename string) (*DM2Demo, error) {
 	}
 	demo := &DM2Demo{binaryData: data}
 	demo.textProto = &pb.DM2Demo{}
+	demo.currentFrame = &pb.Frame{}
 	return demo, nil
 }
 
@@ -52,6 +55,10 @@ func (demo *DM2Demo) Unmarshal() error {
 // integer containing the size of the lump and then a bunch of individual
 // messages
 func (demo *DM2Demo) nextLump() (message.MessageBuffer, int, error) {
+	// shouldn't happen, but gracefully handle just in case
+	if demo.binaryPosition >= len(demo.binaryData) {
+		return message.MessageBuffer{}, 0, errors.New("trying to read past end of lump")
+	}
 	sizebytes := message.NewMessageBuffer(demo.binaryData[demo.binaryPosition : demo.binaryPosition+4])
 	lumpSize := int(sizebytes.ReadLong())
 	if lumpSize == -1 {
@@ -67,7 +74,6 @@ func (demo *DM2Demo) nextLump() (message.MessageBuffer, int, error) {
 
 // Parse all the messages in a particular chunk of data
 func (demo *DM2Demo) UnmarshalLump(data message.MessageBuffer) error {
-	currentFrame := &pb.Frame{}
 	textpb := demo.textProto
 	for data.Index < len(data.Buffer) {
 		cmd := data.ReadByte()
@@ -77,10 +83,10 @@ func (demo *DM2Demo) UnmarshalLump(data message.MessageBuffer) error {
 			textpb.Serverinfo = serverdata
 		case message.SVCConfigString:
 			cs := ConfigstringToProto(&data)
-			if currentFrame.GetNumber() == 0 {
+			if demo.currentFrame.GetNumber() == 0 {
 				textpb.Configstrings = append(textpb.Configstrings, cs)
 			} else {
-				currentFrame.Configstrings = append(currentFrame.Configstrings, cs)
+				demo.currentFrame.Configstrings = append(demo.currentFrame.Configstrings, cs)
 			}
 		case message.SVCSpawnBaseline:
 			bitmask := data.ParseEntityBitmask()
@@ -89,31 +95,31 @@ func (demo *DM2Demo) UnmarshalLump(data message.MessageBuffer) error {
 			textpb.Baselines = append(textpb.Baselines, baseline)
 		case message.SVCStuffText:
 			stuff := StuffTextToProto(&data)
-			if currentFrame.Number > 0 {
-				currentFrame.Stufftexts = append(currentFrame.Stufftexts, stuff)
+			if demo.currentFrame.Number > 0 {
+				demo.currentFrame.Stufftexts = append(demo.currentFrame.Stufftexts, stuff)
 			}
 		case message.SVCFrame: // includes playerstate and packetentities
 			frame := FrameToProto(&data)
 			textpb.Frames = append(textpb.Frames, frame)
-			currentFrame = frame
+			demo.currentFrame = frame
 		case message.SVCPrint:
 			print := PrintToProto(&data)
-			currentFrame.Prints = append(currentFrame.Prints, print)
+			demo.currentFrame.Prints = append(demo.currentFrame.Prints, print)
 		case message.SVCMuzzleFlash:
 			flash := FlashToProto(&data)
-			currentFrame.Flashes1 = append(currentFrame.Flashes1, flash)
+			demo.currentFrame.Flashes1 = append(demo.currentFrame.Flashes1, flash)
 		case message.SVCTempEntity:
 			te := TempEntToProto(&data)
-			currentFrame.TemporaryEntities = append(currentFrame.TemporaryEntities, te)
+			demo.currentFrame.TemporaryEntities = append(demo.currentFrame.TemporaryEntities, te)
 		case message.SVCLayout:
 			layout := LayoutToProto(&data)
-			currentFrame.Layouts = append(currentFrame.Layouts, layout)
+			demo.currentFrame.Layouts = append(demo.currentFrame.Layouts, layout)
 		case message.SVCSound:
 			sound := SoundToProto(&data)
-			currentFrame.Sounds = append(currentFrame.Sounds, sound)
+			demo.currentFrame.Sounds = append(demo.currentFrame.Sounds, sound)
 		case message.SVCCenterPrint:
 			cp := CenterPrintToProto(&data)
-			currentFrame.Centerprints = append(currentFrame.Centerprints, cp)
+			demo.currentFrame.Centerprints = append(demo.currentFrame.Centerprints, cp)
 		}
 	}
 	return nil
@@ -137,4 +143,47 @@ func (demo *DM2Demo) WriteTextProto(filename string) error {
 
 func (demo *DM2Demo) GetTextProto() *pb.DM2Demo {
 	return demo.textProto
+}
+
+// Convert the textproto demo back into a quake 2 playable binary demo. The
+// returned byte slice just needs to be written to a file as is.
+func (demo *DM2Demo) Marshal() ([]byte, error) {
+	out := message.MessageBuffer{}  // the overall demo
+	lump := message.MessageBuffer{} // the current lump
+
+	lump.Append(ServerDataToBinary(demo.textProto.Serverinfo))
+	for _, cs := range demo.GetTextProto().GetConfigstrings() {
+		tmp := ConfigstringToBinary(cs)
+		buildDemoBuffer(&out, &lump, tmp, false)
+	}
+	for _, bl := range demo.GetTextProto().GetBaselines() {
+		tmp := message.MessageBuffer{Buffer: []byte{SvcSpawnBaseline}}
+		tmp.Append(EntityToBinary(bl))
+		buildDemoBuffer(&out, &lump, tmp, false)
+	}
+	precache := StuffTextToBinary(&pb.StuffText{String_: "precache\n"})
+	tmp := message.MessageBuffer{Buffer: []byte{SvcStuffText}}
+	tmp.Append(precache)
+	buildDemoBuffer(&out, &lump, tmp, false)
+
+	// each frame is a new lump at this point
+	for _, fr := range demo.GetTextProto().GetFrames() {
+		tmp := FrameToBinary(fr)
+		buildDemoBuffer(&out, &lump, tmp, true)
+	}
+	out.WriteLong(-1) // end of demo
+	return out.Buffer, nil
+}
+
+// append msg to lump until it can't fit anymore, then append lump to final.
+// Each lump is prefixed with its length (4 bytes).
+//
+// If force is true don't wait until the buffer is full, write it and reset.
+func buildDemoBuffer(final *message.MessageBuffer, lump *message.MessageBuffer, msg message.MessageBuffer, force bool) {
+	if ((len(lump.Buffer) + len(msg.Buffer)) > message.MaxMessageLength) || force {
+		final.WriteLong(int32(len(lump.Buffer)))
+		final.Append(*lump)
+		lump.Reset()
+	}
+	lump.Append(msg)
 }
