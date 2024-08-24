900b0affc8b13a0ecd07d38e034e2af266d94d41
Joe Reid <joe@joereid.com>
2024-08-24 19:55:10 -0400

    Add a test for pak.ExtractFile

--- pak/pak_proto_test.go
+++ pak/pak_proto_test.go
@@ -1,8 +1,11 @@
 package pak
 
 import (
+	"bytes"
 	"os"
 	"testing"
+
+	pb "github.com/packetflinger/libq2/proto"
 )
 
 func TestUnmarshal(t *testing.T) {
@@ -36,5 +39,43 @@ func TestMarshal(t *testing.T) {
 	err = os.WriteFile("../testdata/test-out.pak", data, 0777)
 	if err != nil {
 		t.Error(err)
+	}
+}
+
+func TestExtractFile(t *testing.T) {
+	tests := []struct {
+		name string
+		pak  *pb.PAKArchive
+		file string
+		want []byte
+	}{
+		{
+			name: "Test 1",
+			pak: &pb.PAKArchive{
+				Files: []*pb.PAKFile{
+					{
+						Name: "test.cfg",
+						Data: []byte{1, 2, 3, 4, 5},
+					},
+					{
+						Name: "test2.cfg",
+						Data: []byte{6, 7, 8, 9},
+					},
+				},
+			},
+			file: "test2.cfg",
+			want: []byte{6, 7, 8, 9},
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := ExtractFile(tc.pak, tc.file)
+			if err != nil {
+				t.Error()
+			}
+			if !bytes.Equal(got.Data, tc.want) {
+				t.Error("\ngot:\n", got.Data, "\nwant\n", tc.want)
+			}
+		})
 	}
 }
