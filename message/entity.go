c086bdf4b9fea1c14d9faacdd16a0b1c94f0ab2c
Joe Reid <joe@joereid.com>
2024-12-22 21:50:11 -0500

    Move ParseDeltaEntityBitmask

---
+++ message/entity.go
@@ -0,0 +1,22 @@
+package message
+
+// Read up to the first 4 bytes of an entity, depending on the
+// previous ones. This value tells you what data is in the rest
+// of the entity message.
+func (m *Buffer) ParseEntityBitmask() uint32 {
+	bits := uint32(m.ReadByte())
+
+	if bits&EntityMoreBits1 != 0 {
+		bits |= (uint32(m.ReadByte()) << 8)
+	}
+
+	if bits&EntityMoreBits2 != 0 {
+		bits |= (uint32(m.ReadByte()) << 16)
+	}
+
+	if bits&EntityMoreBits3 != 0 {
+		bits |= (uint32(m.ReadByte()) << 24)
+	}
+
+	return uint32(bits)
+}
--- message/parse.go
+++ message/parse.go
@@ -442,27 +442,6 @@ func (m *Buffer) ParseSpawnBaseline() *pb.PackedEntity {
 	return m.ParseEntity(&pb.PackedEntity{}, number, bitmask)
 }
 
-// Read up to the first 4 bytes of an entity, depending on the
-// previous ones. This value tells you what data is in the rest
-// of the entity message.
-func (m *Buffer) ParseEntityBitmask() uint32 {
-	bits := uint32(m.ReadByte())
-
-	if bits&EntityMoreBits1 != 0 {
-		bits |= (uint32(m.ReadByte()) << 8)
-	}
-
-	if bits&EntityMoreBits2 != 0 {
-		bits |= (uint32(m.ReadByte()) << 16)
-	}
-
-	if bits&EntityMoreBits3 != 0 {
-		bits |= (uint32(m.ReadByte()) << 24)
-	}
-
-	return uint32(bits)
-}
-
 func (m *Buffer) ParseEntityNumber(flags uint32) uint16 {
 	num := uint16(0)
 	if flags&EntityNumber16 != 0 {
