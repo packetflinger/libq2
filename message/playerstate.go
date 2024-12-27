d551b6c4a5b8145213b8260b24121ffd113fec31
Joe Reid <joe@joereid.com>
2024-12-27 08:01:02 -0500

    Rename bits to mask in DeltaPlayerBitmask

--- message/playerstate.go
+++ message/playerstate.go
@@ -13,57 +13,57 @@ import (
 //
 // The bitmask is an unsigned short
 func DeltaPlayerBitmask(from *pb.PackedPlayer, to *pb.PackedPlayer) uint16 {
-	bits := uint16(0)
+	mask := uint16(0)
 	mf := from.GetMovestate()
 	mt := to.GetMovestate()
 
 	if mf.GetType() != mt.GetType() {
-		bits |= PlayerType
+		mask |= PlayerType
 	}
 
 	if mf.GetOriginX() != mt.GetOriginX() || mf.GetOriginY() != mt.GetOriginY() || mf.GetOriginZ() != mt.GetOriginZ() {
-		bits |= PlayerOrigin
+		mask |= PlayerOrigin
 	}
 	if mf.GetVelocityX() != mt.GetVelocityX() || mf.GetVelocityY() != mt.GetVelocityY() || mf.GetVelocityZ() != mt.GetVelocityZ() {
-		bits |= PlayerVelocity
+		mask |= PlayerVelocity
 	}
 	if mf.GetTime() != mt.GetTime() {
-		bits |= PlayerTime
+		mask |= PlayerTime
 	}
 	if mf.GetFlags() != mt.GetFlags() {
-		bits |= PlayerFlags
+		mask |= PlayerFlags
 	}
 	if mf.GetGravity() != mt.GetGravity() {
-		bits |= PlayerGravity
+		mask |= PlayerGravity
 	}
 	if mf.GetDeltaAngleX() != mt.GetDeltaAngleX() || mf.GetDeltaAngleY() != mt.GetDeltaAngleY() || mf.GetDeltaAngleZ() != mt.GetDeltaAngleZ() {
-		bits |= PlayerDeltaAngles
+		mask |= PlayerDeltaAngles
 	}
 	if from.GetViewOffsetX() != to.GetViewOffsetX() || from.GetViewOffsetY() != to.GetViewOffsetY() || from.GetViewOffsetZ() != to.GetViewOffsetZ() {
-		bits |= PlayerViewOffset
+		mask |= PlayerViewOffset
 	}
 	if from.GetViewAnglesX() != to.GetViewAnglesX() || from.GetViewAnglesY() != to.GetViewAnglesY() || from.GetViewAnglesZ() != to.GetViewAnglesZ() {
-		bits |= PlayerViewAngles
+		mask |= PlayerViewAngles
 	}
 	if from.GetKickAnglesX() != to.GetKickAnglesX() || from.GetKickAnglesY() != to.GetKickAnglesY() || from.GetKickAnglesZ() != to.GetKickAnglesZ() {
-		bits |= PlayerKickAngles
+		mask |= PlayerKickAngles
 	}
 	if from.GetBlendW() != to.GetBlendW() || from.GetBlendX() != to.GetBlendX() || from.GetBlendY() != to.GetBlendY() || from.GetBlendZ() != to.GetBlendZ() {
-		bits |= PlayerBlend
+		mask |= PlayerBlend
 	}
 	if from.GetFov() != to.GetFov() {
-		bits |= PlayerFOV
+		mask |= PlayerFOV
 	}
 	if from.GetRdFlags() != to.GetRdFlags() {
-		bits |= PlayerRDFlags
+		mask |= PlayerRDFlags
 	}
 	if from.GetGunFrame() != to.GetGunFrame() || from.GetGunOffsetX() != to.GetGunOffsetX() || from.GetGunOffsetY() != to.GetGunOffsetY() || from.GetGunOffsetZ() != to.GetGunOffsetZ() || from.GetGunAnglesX() != to.GetGunAnglesX() || from.GetGunAnglesY() != to.GetGunAnglesY() || from.GetGunAnglesZ() != to.GetGunAnglesZ() {
-		bits |= PlayerWeaponFrame
+		mask |= PlayerWeaponFrame
 	}
 	if from.GetGunIndex() != to.GetGunIndex() {
-		bits |= PlayerWeaponIndex
+		mask |= PlayerWeaponIndex
 	}
-	return bits
+	return mask
 }
 
 // ParseDeltaPlayerstate will merge a previous playerstate with one parsed
