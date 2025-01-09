7cfe3ad02c5a730c25ea357807d2fc3563935c88
Joe Reid <joe@joereid.com>
2025-01-09 13:07:13 -0500

    Add NewServer() and tests

--- state/server.go
+++ state/server.go
@@ -1,7 +1,28 @@
 package state
 
+import (
+	"fmt"
+	"net"
+	"strconv"
+)
+
 type Server struct {
 	Address  string // IP or DNS name
 	Port     int
 	Password string // rcon
+}
+
+func NewServer(srv string) (Server, error) {
+	if srv == "" {
+		return Server{}, fmt.Errorf("NewServer(): empty input")
+	}
+	address, p, err := net.SplitHostPort(srv)
+	if err != nil {
+		return Server{}, fmt.Errorf("error splitting host and port: %v", err)
+	}
+	port, err := strconv.ParseInt(p, 10, 16)
+	if err != nil {
+		return Server{}, fmt.Errorf("NewServer(%q): invalid port, number 1-65535 required", srv)
+	}
+	return Server{Address: address, Port: int(port)}, nil
 }
---
+++ state/server_test.go
@@ -0,0 +1,67 @@
+package state
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestNewServer(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   string
+		want    Server
+		wantErr bool
+	}{
+		{
+			name:    "empty",
+			input:   "",
+			want:    Server{},
+			wantErr: true,
+		},
+		{
+			name:    "no port",
+			input:   "1.2.3.4",
+			want:    Server{},
+			wantErr: true,
+		},
+		{
+			name:  "IPv4",
+			input: "1.2.3.4:27999",
+			want: Server{
+				Address: "1.2.3.4",
+				Port:    27999,
+			},
+			wantErr: false,
+		},
+		{
+			name:  "hostname no port",
+			input: "test.srv.addr:27910",
+			want: Server{
+				Address: "test.srv.addr",
+				Port:    27910,
+			},
+			wantErr: false,
+		},
+		{
+			name:  "IPv6 with port",
+			input: "[2003:db8::3]:27988",
+			want: Server{
+				Address: "2003:db8::3",
+				Port:    27988,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := NewServer(tc.input)
+			if err != nil && !tc.wantErr {
+				t.Errorf("NewServer(%v) returned unexpected error: %v\n", tc.input, err)
+			}
+			if diff := cmp.Diff(got, tc.want); diff != "" {
+				t.Errorf("NewServer(%v) diff: %v\n", tc.input, diff)
+			}
+		})
+	}
+}
