716b9e62e12d2f70484ad3f080bf3be741e3b295
Joe Reid <joe@joereid.com>
2026-08-03 22:33:52 -0400

    Add location funcs to state package

---
+++ state/location_test.go
@@ -0,0 +1,52 @@
+package state
+
+import (
+	"testing"
+)
+
+func TestCountry(t *testing.T) {
+	tests := []struct {
+		name string
+		addr string
+		want string
+	}{
+		{
+			name: "localhost",
+			addr: "127.0.0.5",
+			want: "zz",
+		},
+		{
+			name: "rfc1918-1",
+			addr: "10.3.5.6",
+			want: "zz",
+		},
+		{
+			name: "pf chicago server",
+			addr: "169.197.131.131",
+			want: "us",
+		},
+		{
+			name: "pf germany server",
+			addr: "86.105.53.128",
+			want: "de",
+		},
+		{
+			name: "pf au server",
+			addr: "103.73.64.180",
+			want: "au",
+		},
+	}
+
+	IPCountryList, err := LoadIPCountries("/tmp/ipcountry.csv", false)
+	if err != nil {
+		t.Fatalf("unable to load country db: %v", err)
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got := (&IPCountryList).Country(tc.addr)
+			if got != tc.want {
+				t.Errorf("Country(%q) = %q, want %q", tc.addr, got, tc.want)
+			}
+		})
+	}
+}
