2ff79587f8d17fa60213369c27a8923cf5e9860d
Joe Reid <joe@joereid.com>
2026-08-10 10:22:50 -0400

    Comments and nil reference checks

--- pak/pak.go
+++ pak/pak.go
@@ -1,3 +1,43 @@
+// A .pak file is a container for a collection of files similar to a .zip but
+// with no compression. Used for implementing a very simple virual filesystem
+// in Quake 2. Multiple PAK files are usually defined and their contents are
+// combined into a single virtual filesystem.
+//
+// All Quake 2 clients support PAK files, as it was the original archive format
+// included when the game was created. The q2pro client supports .pkz files
+// which are basically just .zip files with a different extension. This is the
+// much preferred archive format as it's natively supported in nearly all OSes.
+//
+// This implementation does not allow for duplicate files in the same archive.
+// If you try to add a file of the same name as an existing file in the pak an
+// error will surface. If a file is added with the same contents but under a
+// different name, the index will point to the same data location for both in
+// an effort to minimize waste.
+//
+// Structure -
+// PAK files consist of a header, a blob of all the file contents concatinated
+// together, and an index of the file paths and locations where the data for
+// those files can be found within the blob.
+//
+//	Entire file:
+//	  [ header ][ all file contents blob ][ index ]
+//	Header:
+//	  [ magic 4-bytes ][ index location 4-bytes ][ index length 4-bytes ]
+//	Index:
+//	  [ filename 56-bytes ][ data location 4-bytes ][ data len 4 bytes ]
+//
+// Limitations -
+// File paths can only be upto 56 characters long. An index entry is 64 bytes
+// with the data location being 4 bytes and the data length also being 4 bytes.
+// The remaining 56 byte can be used for the file's path/name. Any unused space
+// in the name is padded with nulls.
+//
+// There is a hard-limit 4GB for a PAK file. This limition applies for both the
+// overall size of the .pak file and any individual file contained inside the
+// container. This is due to the internal addressing using 32-bit integers for
+// data lengths and locations. The header contains the offset for where the
+// index is located at the end of the file; that offset at can at most be
+// ~4,294,000,000 bytes +/- depending on the number of files in the index.
 package pak
 
 import (
@@ -17,6 +57,15 @@ const (
 	Separator       = "/" // always use linux-style, even on windows
 )
 
+var (
+	ErrorDuplicateName = errors.New("Duplicate file name in pak")
+	ErrorFileNotFound  = errors.New("File not found in pak")
+	ErrorNilPak        = errors.New("Nil pak archive")
+)
+
+// Load the binary .pak file data into a structured text proto. This structure
+// can be easily modified (changing/adding/removing files) and then changed
+// back into a binary .pak file.
 func Unmarshal(data []byte) (*pb.PAKArchive, error) {
 	header := message.NewBuffer(data[:HeaderLength])
 	if header.ReadLong() != Magic {
@@ -45,8 +94,12 @@ func Unmarshal(data []byte) (*pb.PAKArchive, error) {
 	return pak, nil
 }
 
-// generate a binary pak, it should be written to disk after.
+// Generate a binary .pak from the text proto. The data can then be written to
+// to disk for a fully materialized .pak file.
 func Marshal(archive *pb.PAKArchive) ([]byte, error) {
+	if archive == nil {
+		return []byte{}, ErrorNilPak
+	}
 	var buf, dataLump, metaLump message.Buffer
 	for _, f := range archive.GetFiles() {
 		metaLump.WriteString(f.Name)
@@ -65,18 +118,29 @@ func Marshal(archive *pb.PAKArchive) ([]byte, error) {
 	return buf.Data, nil
 }
 
-// Add a new file to the contents of the PAK. You'll need to have already
-// os.ReadFile()'d to get the data.
-func AddFiles(archive *pb.PAKArchive, name string, data []byte) {
+// Add a new file virtual file to the contents of the PAK.
+func AddFile(archive *pb.PAKArchive, name string, data []byte) error {
+	if archive == nil {
+		return ErrorNilPak
+	}
+	for _, f := range archive.GetFiles() {
+		if f.GetName() == name {
+			return ErrorDuplicateName
+		}
+	}
 	newfile := &pb.PAKFile{
 		Name: name,
 		Data: data,
 	}
 	archive.Files = append(archive.Files, newfile)
+	return nil
 }
 
 // Delete a file contained in a PAK archive
 func RemoveFile(archive *pb.PAKArchive, name string) error {
+	if archive == nil {
+		return ErrorNilPak
+	}
 	files := []*pb.PAKFile{}
 	for _, f := range archive.GetFiles() {
 		if f.GetName() == name {
@@ -85,7 +149,7 @@ func RemoveFile(archive *pb.PAKArchive, name string) error {
 		files = append(files, f)
 	}
 	if len(files) == len(archive.GetFiles()) {
-		return errors.New("file not in PAK archive")
+		return ErrorFileNotFound
 	}
 	archive.Files = files
 	return nil
@@ -93,10 +157,13 @@ func RemoveFile(archive *pb.PAKArchive, name string) error {
 
 // Obtain a pointer to a file contained in a PAK archive.
 func ExtractFile(archive *pb.PAKArchive, name string) (*pb.PAKFile, error) {
+	if archive == nil {
+		return nil, ErrorNilPak
+	}
 	for _, f := range archive.GetFiles() {
 		if f.Name == name {
 			return f, nil
 		}
 	}
-	return nil, errors.New("file not found in PAK: " + name)
+	return nil, ErrorFileNotFound
 }
